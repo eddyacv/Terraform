30b7f5e3cc5c3fb3ccce10a60d014fefc34ac7ea
Dylan <dylan.acevedo2843@gmail.com>
2025-04-01 21:10:20 -0500

    Outputs

---
+++ Outputs/output.tf
@@ -0,0 +1,10 @@
+output "nombre_rg_output" {
+  value = azurerm_resource_group.rg.id
+  
+}
+output "nombre_rg2_output" {
+  value = azurerm_resource_group.rg2.id
+}
+output "nombre_rg3_output" {
+  value = azurerm_resource_group.rg3.id
+}
---
+++ Outputs/variables.tf
@@ -0,0 +1,17 @@
+variable "rg_nombre" {
+  description = "Nombre del grupo de recursos"
+  type        = string
+  
+}
+
+variable "rg_nombre2" {
+  description = "Nombre del grupo de recursos 2"
+  type        = string
+  
+}
+
+variable "rg_nombre3" {
+  description = "Nombre del grupo de recursos 3"
+  type        = string
+  
+}
